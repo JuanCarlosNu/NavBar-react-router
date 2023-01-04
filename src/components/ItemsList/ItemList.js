7b042733d61284f8de7c82ddde51fff795d0d44d
Juan Carlos Nuñez <juancarlosm.nunez@gmail.com>
2023-01-04 17:16:19 -0300

    filter courses by title

--- src/components/ItemsList/ItemList.js
+++ src/components/ItemsList/ItemList.js
@@ -7,26 +7,33 @@ class ItemList extends React.Component {
   constructor(){
     super()
     this.state= {
-      courses
+      courses,
+      inputSearch: "",
+
 
     }
 
   }
 handleChange = event=> {
-  console.log(event.target.value)
+  //console.log(event.target.value)
+  this.setState({
+    inputSearch: event.target.value
+  });
+  console.log(this.state.inputSearch);
  } 
+ 
   render(){ 
-
+    const filtered = this.state.courses.filter(c => c.title.includes(this.state.inputSearch))
      return (
      <div className="ItemList">
       <form className="search__form">
-        <input className="search__input" type="text" placeholder="search course" onChange={this.handleChange}></input>
+        <input className="search__input" type="text" placeholder="search course" onChange={this.handleChange} value={this.state.inputSearch}></input>
       </form>
      
 
     <div className="courses">
 
-      {this.state.courses.map(course => (
+      {filtered.map(course => (
         <Item key={course.id} course={course} />
       ))}
     </div>
