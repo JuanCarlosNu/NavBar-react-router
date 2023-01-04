5928d351b8b338484e3744414077667107603edb
Juan Carlos Nuñez <juancarlosm.nunez@gmail.com>
2023-01-04 15:45:07 -0300

    input search

--- src/components/ItemsList/ItemList.js
+++ src/components/ItemsList/ItemList.js
@@ -12,14 +12,26 @@ class ItemList extends React.Component {
     }
 
   }
- 
-  render(){  return (
+handleChange = event=> {
+  console.log(event.target.value)
+ } 
+  render(){ 
+
+     return (
+     <div className="ItemList">
+      <form className="search__form">
+        <input className="search__input" type="text" placeholder="search course" onChange={this.handleChange}></input>
+      </form>
+     
+
     <div className="courses">
+
       {this.state.courses.map(course => (
         <Item key={course.id} course={course} />
       ))}
     </div>
-  )}  ;
+    </div>
+  )};
 };
 
 export default ItemList;
